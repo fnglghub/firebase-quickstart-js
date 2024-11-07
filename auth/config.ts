c07180651e56317d1024568ae05d391a13ae7c53
fnglghub <founder@newgenlearning.co.uk>
2024-11-07 19:44:27 +0000

    Update config.ts
    ///The correct values in google doc. Before publishing to Production apply correct values.

--- auth/config.ts
+++ auth/config.ts
@@ -1,3 +1,5 @@
+///The correct values in google doc. Before publishing to Production apply correct values.
+
 export const firebaseConfig = {
   // Your web app's Firebase configuration here
   // See https://firebase.google.com/docs/web/setup#add-sdks-initialize
